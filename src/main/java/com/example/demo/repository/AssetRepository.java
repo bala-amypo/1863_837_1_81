2870c8c881ce4a42a76e0966e135f19fa4aa4cf6
bala-amypo <bala@amypo.in>
2025-12-22 17:13:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetRepository.java
+++ src/main/java/com/example/demo/repository/AssetRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
-@Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+
+    Optional<Asset> findByAssetTag(String assetTag);
+
+    List<Asset> findByStatus(String status);
 }
--- src/main/java/com/example/demo/repository/DisposalRecordRepository.java
+++ src/main/java/com/example/demo/repository/DisposalRecordRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DisposalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
 public interface DisposalRecordRepository extends JpaRepository<DisposalRecord, Long> {
+
+    Optional<DisposalRecord> findByAsset_Id(Long assetId);
 }
